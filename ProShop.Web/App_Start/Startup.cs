0231a1eb56869a27e493af93a04322970509db87
Hung6066 <thanhhung6066@gmail.com>
2018-08-28 14:30:08 +0700

    add dependency injection autofac web

---
+++ ProShop.Web/App_Start/Startup.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+using Autofac;
+using PrShop.Data.Infrastructure;
+using PrShop.Data;
+using PrShop.Data.Repositories;
+using PrShop.Service;
+using System.Web.Mvc;
+using Autofac.Integration.Mvc;
+using System.Web.Http;
+using Autofac.Integration.WebApi;
+using System.Reflection;
+
+[assembly: OwinStartup(typeof(ProShop.Web.App_Start.Startup))]
+
+namespace ProShop.Web.App_Start
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigAutofac(app);
+        }
+        private void ConfigAutofac(IAppBuilder app)
+        {
+            var builder = new ContainerBuilder();
+
+            builder.RegisterControllers(Assembly.GetExecutingAssembly());
+            //Register your Web API controllers.
+            builder.RegisterApiControllers(Assembly.GetExecutingAssembly());
+
+            builder.RegisterType<UnitOfWork>().As<IUnitOfWork>().InstancePerRequest();
+            builder.RegisterType<DbFactory>().As<IDbFactory>().InstancePerRequest();
+
+            builder.RegisterType<PrShopDbContext>().AsSelf().InstancePerRequest();
+
+            //Repositories
+            builder.RegisterAssemblyTypes(typeof(PostCategoryRepository).Assembly)
+                .Where(t => t.Name.EndsWith("Repository"))
+                .AsImplementedInterfaces().InstancePerRequest();
+            
+            //Service
+            builder.RegisterAssemblyTypes(typeof(PostCategoryService).Assembly)
+                .Where(t => t.Name.EndsWith("Service"))
+                .AsImplementedInterfaces().InstancePerRequest();
+
+            IContainer container = builder.Build();
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
+
+            GlobalConfiguration.Configuration.DependencyResolver = new AutofacWebApiDependencyResolver(container);
+        }
+    }
+}
