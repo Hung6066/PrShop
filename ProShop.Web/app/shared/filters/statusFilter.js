c3e2c7012c5674e99df0fad7f2d89e0ea86c5527
Hung6066 <thanhhung6066@gmail.com>
2018-08-30 10:08:58 +0700

    add filter status and format data in list

---
+++ ProShop.Web/app/shared/filters/statusFilter.js
@@ -0,0 +1,11 @@
+﻿
+(function (app) {
+    app.filter('statusFilter', function () {
+        return function (intput) {
+            if (intput)
+                return 'Kích hoạt';
+            else
+                return 'Khóa';
+        }
+    });
+})(angular.module('prshop.common'));
