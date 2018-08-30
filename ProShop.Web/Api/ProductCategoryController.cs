9e9236ef61de1e322dfc8b450a349e29fcf5785d
Hung6066 <thanhhung6066@gmail.com>
2018-08-30 11:08:23 +0700

    paging for list

--- ProShop.Web/Api/ProductCategoryController.cs
+++ ProShop.Web/Api/ProductCategoryController.cs
@@ -1,34 +1,51 @@
-﻿using PrShop.Web.Infrastructure.Core;
+﻿using AutoMapper;
+using ProShop.Service;
+using ProShop.Web.Infrastructure.Core;
+using ProShop.Web.Models;
+using PrShop.Model.Models;
+using PrShop.Service;
+using PrShop.Web.Infrastructure.Core;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
-using PrShop.Service;
-using ProShop.Service;
-using AutoMapper;
-using PrShop.Model.Models;
-using ProShop.Web.Models;
 
 namespace ProShop.Web.Api
 {
     [RoutePrefix("api/productcategory")]
     public class ProductCategoryController : ApiControllerBase
     {
-        IProductCategoryService _productCategoryService;
+        private IProductCategoryService _productCategoryService;
+
         public ProductCategoryController(IErrorService errorService, IProductCategoryService productCategoryService) : base(errorService)
         {
             _productCategoryService = productCategoryService;
         }
+
         [Route("getall")]
-        public HttpResponseMessage GetAll(HttpRequestMessage request)
+        public HttpResponseMessage GetAll(HttpRequestMessage request, int page, int pageSize = 20)
         {
             return CreateHttpResponse(request, () =>
             {
+                var totalRow = 0;
                 var model = _productCategoryService.GetAll();
-                var responseData = Mapper.Map<IEnumerable<ProductCategory>, IEnumerable<ProductCategoryViewModel>>(model);
-                var response = request.CreateResponse(HttpStatusCode.OK, responseData);
+
+                totalRow = model.Count();
+                var query = model.OrderByDescending(x => x.CreateDate).Skip(page * pageSize).Take(pageSize);
+
+                var responseData = Mapper.Map<IEnumerable<ProductCategory>, IEnumerable<ProductCategoryViewModel>>(query);
+
+                var paginationSet = new PaginationSet<ProductCategoryViewModel>()
+                {
+                    Items = responseData,
+                    Page = page,
+                    TotalCount = totalRow,
+                    TotalPages = (int)Math.Ceiling((decimal)totalRow / pageSize)
+                };
+
+                var response = request.CreateResponse(HttpStatusCode.OK, paginationSet);
                 return response;
             });
         }
